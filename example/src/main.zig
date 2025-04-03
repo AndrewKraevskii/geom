0e7d80a4f18456db59da666a400b1ef429802d7c
andrewkraevskii <andrew.kraevskii@gmail.com>
2025-04-03 19:51:06 +0300

    3d vis

--- example/src/main.zig
+++ example/src/main.zig
@@ -15,11 +15,20 @@ pub fn main() !void {
     rl.initWindow(720, 480, "geo math");
     defer rl.closeWindow();
 
-    var camera: rl.Camera2D = .{
+    var camera: rl.Camera3D = .{
         .target = .zero(),
-        .offset = .{ .x = @as(f32, @floatFromInt(rl.getScreenWidth())) / 2, .y = @as(f32, @floatFromInt(rl.getScreenHeight())) / 2 },
-        .zoom = 1,
-        .rotation = 0,
+        .position = .{
+            .x = -10,
+            .y = 10,
+            .z = -10,
+        },
+        .up = .{
+            .x = 0,
+            .y = 1,
+            .z = 0,
+        },
+        .fovy = 60,
+        .projection = .perspective,
     };
 
     var red_point: Point = .{
@@ -43,18 +52,41 @@ pub fn main() !void {
 
         camera.begin();
         defer camera.end();
+        rl.drawGrid(10, 1);
 
-        // drawAxis(0, 0, 1000, .red);
-        // rl.drawText("geo math", 0, 0, 10, .red);
+        const ray = rl.getScreenToWorldRay(rl.getMousePosition(), camera);
 
-        const vec = rl.getScreenToWorld2D(rl.getMousePosition(), camera);
+        const collision = rl.getRayCollisionQuad(
+            ray,
+            .{
+                .x = -10,
+                .y = 0,
+                .z = -10,
+            },
+            .{
+                .x = 10,
+                .y = 0,
+                .z = -10,
+            },
+            .{
+                .x = 10,
+                .y = 0,
+                .z = 10,
+            },
+            .{
+                .x = -10,
+                .y = 0,
+                .z = 10,
+            },
+        );
 
+        // @compileLog(@typeInfo(@TypeOf(vec)).@"struct".fields);
         const mouse_pos: Point =
             .{
                 .e123 = 1,
-                .e023 = vec.x,
-                .e013 = vec.y,
-                .e012 = 0,
+                .e023 = collision.point.x,
+                .e013 = collision.point.y,
+                .e012 = collision.point.z,
             };
 
         if (rl.isMouseButtonDown(.left)) {
@@ -75,74 +107,64 @@ pub fn main() !void {
                 join(blue_point, red_point),
                 @floatCast((@sin(rl.getTime()) + 1) / 2),
             );
-
             const green_point = sandwich(mult(white_line, pink_line), red_point);
             const yellow_point = project(white_line, red_point);
 
             drawTriangle(.{ green_point, yellow_point, blue_point }, .magenta);
-            drawLine(white_line, .white);
-            drawLine(join(blue_point, red_point), .red);
-            drawLine(join(green_point, red_point), .red);
-            drawLine(pink_line, .pink);
+            // drawLine(white_line, .white);
+            // drawLine(join(blue_point, red_point), .red);
+            // drawLine(join(green_point, red_point), .red);
+            // drawLine(pink_line, .pink);
 
             drawPoint(green_point, .green);
             drawPoint(yellow_point, .yellow);
             drawPoint(red_point, .red);
             drawPoint(blue_point, .blue);
-            drawLine(meet(geo.Plane{
-                .e2 = 0,
-                .e1 = 0,
-                .e3 = 1,
-                .e0 = 0,
-            }, geo.dual(blue_point)), .blue);
+            drawLineSegment(blue_point, red_point, .red);
+            // drawLine(meet(geo.Plane{
+            //     .e2 = 0,
+            //     .e1 = 0,
+            //     .e3 = 1,
+            //     .e0 = 0,
+            // }, geo.dual(blue_point)), .blue);
         }
     }
 }
 
 pub fn drawPoint(point: Point, color: Color) void {
-    const vec: rl.Vector2 = .{
+    const vec: rl.Vector3 = .{
         .x = point.e023 / point.e123,
         .y = point.e013 / point.e123,
+        .z = point.e012 / point.e123,
     };
-    rl.drawCircleV(vec, 10, color);
+    rl.drawSphere(vec, 1, color);
 }
 
-pub fn drawLine(line: geo.Line, color: Color) void {
-    const left_side_of_screen = geo.Plane{
-        .e2 = -1,
-        .e1 = 0,
-        .e3 = 0,
-        .e0 = 1000,
-    };
+// pub fn drawLine(line: geo.Line, color: Color) void {
+//     rl.drawLine3D(toRaylibPoint(start), toRaylibPoint(end), color);
+// }
 
-    const right_side_of_screen = geo.Plane{
-        .e2 = 1,
-        .e1 = 0,
-        .e3 = 0,
-        .e0 = 1000,
-    };
-
-    const start = geo.meet(left_side_of_screen, line);
-    const end = geo.meet(right_side_of_screen, line);
-
-    rl.drawLineV(toRaylibPoint(start), toRaylibPoint(end), color);
+pub fn drawLineSegment(start: geo.Point, end: geo.Point, color: Color) void {
+    std.debug.print("start: {any} end: {any}\n", .{ toRaylibPoint(start), toRaylibPoint(end) });
+    rl.drawLine3D(toRaylibPoint(start), toRaylibPoint(end), color);
 }
 
-pub fn toRaylibPoint(p: Point) rl.Vector2 {
+pub fn toRaylibPoint(p: Point) rl.Vector3 {
     return .{
         .x = p.e023 / p.e123,
         .y = p.e013 / p.e123,
+        .z = p.e012 / p.e123,
     };
 }
 
 pub fn drawTriangle(points: [3]Point, color: Color) void {
-    rl.drawTriangle(
+    rl.drawTriangle3D(
         toRaylibPoint(points[0]),
         toRaylibPoint(points[1]),
         toRaylibPoint(points[2]),
         color,
     );
-    rl.drawTriangle(
+    rl.drawTriangle3D(
         toRaylibPoint(points[1]),
         toRaylibPoint(points[0]),
         toRaylibPoint(points[2]),
@@ -150,7 +172,7 @@ pub fn drawTriangle(points: [3]Point, color: Color) void {
     );
 }
 
-pub fn drawAxis(x: f32, y: f32, size: f32, color: Color) void {
-    rl.drawLineV(.{ .x = x, .y = y - size / 2 }, .{ .x = x, .y = y + size / 2 }, color);
-    rl.drawLineV(.{ .x = x - size / 2, .y = y }, .{ .x = x + size / 2, .y = y }, color);
-}
+// pub fn drawAxis(x: f32, y: f32, size: f32, color: Color) void {
+//     rl.drawLineV(.{ .x = x, .y = y - size / 2 }, .{ .x = x, .y = y + size / 2 }, color);
+//     rl.drawLineV(.{ .x = x - size / 2, .y = y }, .{ .x = x + size / 2, .y = y }, color);
+// }
